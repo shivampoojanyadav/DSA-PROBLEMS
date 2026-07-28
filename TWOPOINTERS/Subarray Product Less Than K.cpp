7518b3c9573bf92f473881e4cb3a40f5902120c5
shivampoojanyadav <yshivampoojan@gmail.com>
2026-07-28 23:58:24 +0530

    713. Subarray Product Less Than K

---
+++ TWOPOINTERS/Subarray Product Less Than K.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        int count = 0;
+        for(int i = 0; i<nums.size(); i++){
+            int product = 1;
+            for(int j = i; j<nums.size(); j++){
+                product = product*nums[j];
+
+                if(product <k){
+                    count++;
+                }
+
+                else{
+                    break;
+                }
+
+            }
+        }
+
+        return count;
+    }
+};
