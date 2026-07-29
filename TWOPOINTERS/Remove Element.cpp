0b73ce187e279f32331bd3b9b02f72a70c409f14
shivampoojanyadav <yshivampoojan@gmail.com>
2026-07-29 15:16:01 +0530

    27. Remove Element

---
+++ TWOPOINTERS/Remove Element.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int i = 0;
+        for(int j = 0; j<nums.size(); j++){
+            if(nums[j] != val){
+                swap(nums[j], nums[i]);
+                i++;
+            }
+        }
+
+        return i;
+    }
+};
